3218eb18457fb439b88e7979b6384a59104f2f65
Danilo Costa de Carvalho <danilo.advance2011@gmail.com>
2025-09-04 16:50:50 -0300

    chore: add __all__models.py to centralize model imports

---
+++ models/__all__models.py
@@ -0,0 +1,16 @@
+from models.aditivo_nutritivo import AditivoNutritivo
+from models.conservante import Conservante
+from models.ingrediente import Ingrediente
+from models.lote import Lote
+from models.nota_fiscal import NotaFiscal
+from models.picole import Picole
+from models.revendedor import Revendedor
+from models.sabor import Sabor
+from models.tipo_embalagem import TipoEmbalagem
+from models.tipo_picole import TipoPicole
+
+
+from models.aditivos_nutritivos_picole import aditivos_nutritivos_picole
+from models.conservantes_picoles import conservantes_picoles
+from models.ingredientes_picole import ingredientes_picole
+from models.lotes_nota_fiscal import lotes_nota_fiscal
